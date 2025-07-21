609cc5ddd49a5de00d27222da42749aedd70b409
superlakshay <lakshaymaheshwariji@gmail.com>
2025-07-21 13:25:18 +0530

    Delete hms project directory

--- hms project/admin/ajax_approve.php
+++
@@ -1,9 +0,0 @@
-<?php
-include("../connection.php");
-
-$id = $_POST['id'];
-$query = "UPDATE doctors SET status='Approved' WHERE id='$id'";
-mysqli_query($connect,$query);
-
-
-?>
--- hms project/admin/ajax_reject.php
+++
@@ -1,9 +0,0 @@
-<?php
-include("../connection.php");
-
-$id = $_POST['id'];
-$query = "UPDATE doctors SET status='Rejected' WHERE id='$id'";
-mysqli_query($connect,$query);
-
-
-?>
